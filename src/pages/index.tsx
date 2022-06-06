08d9f14ce84015d240ca073b7c599fea9ce14e5d
Luciano Victor <lucianocarvallho7@gmail.com>
2022-06-06 12:19:04 -0300

    feat: add section BannerHero

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,17 @@
-import { IconCircles } from 'assets'
 import { Header } from 'components/Header'
+import { BannerHero } from 'components/BannerHero'
 import Head from 'next/head'
-// import styles from 'styles/home.module.scss'
+
+import styles from 'styles/home.module.scss'
+
 export default function Home() {
   return (
-    <>
+    <div className={styles.container}>
       <Head>
         <title>Digital Agency Enver</title>
       </Head>
       <Header />
-    </>
+      <BannerHero />
+    </div>
   )
 }
