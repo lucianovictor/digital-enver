728e1caed5f1b65b1f44059ae33701f019a45f88
Luciano Victor <lucianocarvallho7@gmail.com>
2022-06-07 13:05:04 -0300

    feat: adding icons export index

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,14 +1,25 @@
-import IconAdd from './Icon/add.svg'
-import IconArrowRightUp from './Icon/arrow-right-up.svg'
-import IconCircles from './Icon/circles.svg'
-import IconLine from './Icon/line.svg'
-import IconPlay from './Icon/play.svg'
-import IconTriangles from './Icon/triangles.svg'
+import IconAdd from './icon/add.svg'
+import IconArrowRightUp from './icon/arrow-right-up.svg'
+import IconCamera from './icon/camera.svg'
+import IconCircles from './icon/circles.svg'
+import IconCode from './icon/code.svg'
+import IconDirection from './icon/direction.svg'
+import IconGrid from './icon/grid.svg'
+import IconLine from './icon/line.svg'
+import IconPlay from './icon/play.svg'
+import IconTriangles from './icon/triangles.svg'
+import IconVideo from './icon/video.svg'
+
 export {
   IconAdd,
   IconArrowRightUp,
+  IconCamera,
   IconCircles,
+  IconCode,
+  IconDirection,
+  IconGrid,
   IconLine,
   IconPlay,
-  IconTriangles
+  IconTriangles,
+  IconVideo
 }
