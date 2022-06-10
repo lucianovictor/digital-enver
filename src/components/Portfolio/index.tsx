5c81b4d6ad54ca5a6d90e2952a53667c1a022be3
Luciano Victor <lucianocarvallho7@gmail.com>
2022-06-10 11:12:34 -0300

    feat: adding update section Portfolio

--- src/components/Portfolio/index.tsx
+++ src/components/Portfolio/index.tsx
@@ -2,8 +2,9 @@ import styles from './styles.module.scss'
 
 export const Portfolio = () => (
   <section>
-    <div>
+    <div className={styles.background}>
       <h1 className={styles.title}>Our Awesome Portofolio</h1>
+
       <div className={styles.projects}>
         <img src="/portfolio/project-1.png" alt="" />
         <img src="/portfolio/project-2.png" alt="" />
