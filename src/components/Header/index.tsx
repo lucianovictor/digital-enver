f7e5d318dab2146636ae4f6bb124a03b8f37716a
Luciano Victor <lucianocarvallho7@gmail.com>
2022-06-13 11:02:01 -0300

    feat: create button toggle

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,36 @@
+import { useState } from 'react'
 import styles from './styles.module.scss'
 
-export const Header = () => (
-  <header>
-    <div className={styles.container}>
-      <img src="/logo.svg" alt="Logo Enver" />
-      <nav>
-        <ul className={styles.navigation}>
-          <li className={styles.active}>Home</li>
-          <li>Services</li>
-          <li>Our Project</li>
-          <li>About us</li>
-        </ul>
-      </nav>
+export const Header = () => {
+  const [activeToggle, setActiveToggle] = useState(false)
 
-      <button type="button">Contact us</button>
-    </div>
-  </header>
-)
+  return (
+    <header>
+      <div className={styles.container}>
+        <img src="/logo.svg" alt="Logo da agência Enver" />
+        <nav>
+          <ul className={styles.navigation}>
+            <li className={styles.active}>Home</li>
+            <li>Services</li>
+            <li>Our Project</li>
+            <li>About us</li>
+          </ul>
+        </nav>
+
+        <button type="button" className={styles.contact}>
+          Contact us
+        </button>
+
+        <button
+          type="button"
+          onClick={() => setActiveToggle(!activeToggle)}
+          className={`${styles.toggle} ${activeToggle ? styles.active : ''}`}
+        >
+          <span />
+          <span />
+          <span />
+        </button>
+      </div>
+    </header>
+  )
+}
