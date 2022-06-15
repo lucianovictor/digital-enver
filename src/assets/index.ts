09ef14cc951dd215433b7394e03ba0977635fee7
Luciano Victor <lucianocarvallho7@gmail.com>
2022-06-15 11:14:28 -0300

    styles: adding images

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,8 @@
 import IconAdd from './icon/add.svg'
 import IconArrowRightUp from './icon/arrow-right-up.svg'
 import IconCamera from './icon/camera.svg'
+import IconChevronLeft from './icon/chevron-left.svg'
+import IconChevronRight from './icon/chevron-right.svg'
 import IconCircles from './icon/circles.svg'
 import IconCode from './icon/code.svg'
 import IconDirection from './icon/direction.svg'
@@ -14,6 +16,8 @@ export {
   IconAdd,
   IconArrowRightUp,
   IconCamera,
+  IconChevronLeft,
+  IconChevronRight,
   IconCircles,
   IconCode,
   IconDirection,
