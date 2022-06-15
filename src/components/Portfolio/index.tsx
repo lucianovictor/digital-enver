4b9090a262fe001279dfae1e9a62176be273c474
Luciano Victor <lucianocarvallho7@gmail.com>
2022-06-15 11:17:10 -0300

    feat: adding updates in the section Portfolio

--- src/components/Portfolio/index.tsx
+++ src/components/Portfolio/index.tsx
@@ -1,15 +1,68 @@
+import { IconChevronLeft, IconChevronRight } from 'assets'
+import { useState } from 'react'
 import styles from './styles.module.scss'
 
-export const Portfolio = () => (
-  <section>
-    <div className={styles.background}>
-      <h1 className={styles.title}>Our Awesome Portofolio</h1>
+const IMAGES = [
+  '/portfolio/project-1.png',
+  '/portfolio/project-2.png',
+  '/portfolio/project-3.png'
+]
 
-      <div className={styles.projects}>
-        <img src="/portfolio/project-1.png" alt="" />
-        <img src="/portfolio/project-2.png" alt="" />
-        <img src="/portfolio/project-3.png" alt="" />
+export const Portfolio = () => {
+  const [activeImage, setActiveImage] = useState(0)
+
+  const handlePrev = () => {
+    const isFirstImage = activeImage <= 0
+
+    if (!isFirstImage) {
+      setActiveImage(activeImage - 1)
+    } else {
+      setActiveImage(0)
+    }
+  }
+
+  const handleNext = () => {
+    const isLastImage = activeImage >= IMAGES.length - 1
+
+    if (!isLastImage) {
+      setActiveImage(activeImage + 1)
+    } else {
+      setActiveImage(IMAGES.length - 1)
+    }
+  }
+
+  return (
+    <section>
+      <div className={styles.background}>
+        <h1 className={styles.title}>Our Awesome Portofolio</h1>
+
+        <div className={styles.projects}>
+          {IMAGES.map((image, index) => (
+            <img
+              src={image}
+              key={image}
+              className={index === activeImage ? styles.active : ''}
+            />
+          ))}
+        </div>
+
+        <div className={styles.controller}>
+          <button
+            type="button"
+            onClick={handlePrev}
+            disabled={activeImage === 0}
+          >
+            <IconChevronLeft />
+          </button>
+          <button
+            type="button"
+            onClick={handleNext}
+            disabled={activeImage === IMAGES.length - 1}
+          >
+            <IconChevronRight />
+          </button>
+        </div>
       </div>
-    </div>
-  </section>
-)
+    </section>
+  )
+}
